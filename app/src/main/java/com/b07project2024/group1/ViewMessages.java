cb1906b523501fbb6ab19bc47dcdab2fcb0f0018
Morgan Huang <morgan56389@gmail.com>
2024-08-01 20:37:20 -0400

    feat: added login

---
+++ app/src/main/java/com/b07project2024/group1/ViewMessages.java
@@ -0,0 +1,7 @@
+package com.b07project2024.group1;
+
+public interface ViewMessages {
+    String success = "Login Successful";
+    String failure = "Login Failed";
+    String incomplete = "Please fill in all fields";
+}
