301ff8d6fa774fcc23ea206d5a87f7a6c199cd3d
JaydenFloof <jayvuong15@gmail.com>
2024-08-06 20:44:43 -0400

    feat: implemented all report types

--- app/src/main/java/com/b07project2024/group1/ReportFragment.java
+++ app/src/main/java/com/b07project2024/group1/ReportFragment.java
@@ -1,15 +1,12 @@
 package com.b07project2024.group1;
 
 import android.Manifest;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.text.Layout;
-import android.text.StaticLayout;
-import android.text.TextPaint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,53 +15,12 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Typeface;
-import android.graphics.pdf.PdfDocument;
-import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.List;
-import java.util.Objects;
-
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-import static com.google.android.gms.common.internal.Asserts.checkNull;
-//TODO Resolve duplicate imports
-
 public class ReportFragment extends Fragment {
-    private CatalogViewModel catalogViewModel;
+    private ReportViewModel reportViewModel = new ReportViewModel();
     Button btn;
-    int pageHeight = 1920;
-    int pagewidth = 1080;
-    int imgHeight = 600;
-    int imgWidth = 750;
-    Bitmap bmp, scaledbmp;
 
     @Nullable
     @Override
@@ -73,14 +29,19 @@ public class ReportFragment extends Fragment {
         StrictMode.setThreadPolicy(policy);
 
         View view = inflater.inflate(R.layout.fragment_report_view, container, false);
-        catalogViewModel = new ViewModelProvider(requireActivity()).get(CatalogViewModel.class);
 
-        EditText lot = view.findViewById(R.id.buttonEditLot);
+        Spinner report = view.findViewById(R.id.spinnerReportType);
+        EditText parameter = view.findViewById(R.id.editTextParameter);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.report_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        report.setAdapter(adapter);
 
         ActivityResultLauncher<String> requestPermissionLauncher =
                 registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
                     if (isGranted) {
-                        Toast.makeText(getActivity(), "Permission acccepted.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getActivity(), "Permission Acccepted.", Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(getActivity(), "Permission Denied.", Toast.LENGTH_SHORT).show();
                     }
@@ -89,141 +50,27 @@ public class ReportFragment extends Fragment {
         requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
 
         btn = view.findViewById(R.id.reportButton);
-//        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.ic_person_24dp);
-//        scaledbmp = Bitmap.createScaledBitmap(bmp, 140, 140, false);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String lotText = lot.getText().toString();
-                Log.d("LOTOT", "Lot is given as " + lotText);
+                String reportText = report.getSelectedItem().toString();
+                String paramText = parameter.getText().toString();
 
-                CatalogItem search = new CatalogItem();
-                search.setLot(lotText);
-                LiveData<List<CatalogItem>> list = catalogViewModel.getLiveList();
+                if(reportText.equals("Generate report by Lot number") && !paramText.isEmpty()){reportViewModel.getWholeCatalog(paramText, "lot");}
+                else if(reportText.equals("Generate report by Name")&& !paramText.isEmpty()){reportViewModel.getWholeCatalog(paramText, "name");}
+                else if(reportText.equals("Generate report by Category")&& !paramText.isEmpty()){reportViewModel.getWholeCatalog(paramText, "category");}
+                else if(reportText.equals("Generate report by Category with Desciption and Picture only")&& !paramText.isEmpty()){reportViewModel.getWholeCatalog(paramText, "categoryDP");}
+                else if(reportText.equals("Generate report by Period")&& !paramText.isEmpty()){reportViewModel.getWholeCatalog(paramText, "period");}
+                else if(reportText.equals("Generate report by Period with Description and Picture only")&& !paramText.isEmpty()){reportViewModel.getWholeCatalog(paramText, "periodDP");}
+                else if(reportText.equals("Generate report for all items")){reportViewModel.getWholeCatalog(paramText, "all");}
+                else if(reportText.equals("Generate report for all items with Description and Picture only")){reportViewModel.getWholeCatalog(paramText, "allDP");}
+                else{Toast.makeText(getActivity(), "Please fill out the parameter field", Toast.LENGTH_SHORT).show();}
 
-                if(list.getValue() != null){
-                    for(int i=0; i<list.getValue().size(); i++){
-                        if(list.getValue().get(i).getLot().equals(search.getLot())){
-                            generatePDF(list.getValue().get(i));
-                            return;
-                        }
-                    }
-                    Log.d("ERRRR", "NOT FOUND");
-                }
+                parameter.setText("");
             }
         });
         return view;
     }
-
-    private void generatePDF(CatalogItem item) {
-        PdfDocument pdfDocument = new PdfDocument();
-        Paint paint = new Paint();
-        TextPaint t = new TextPaint();
-        TextPaint textPaint = new TextPaint();
-
-        PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(pagewidth, pageHeight, 1).create();
-        PdfDocument.Page myPage = pdfDocument.startPage(mypageInfo);
-        Canvas canvas = myPage.getCanvas();
-
-        textPaint.setColor(Color.BLACK );
-        textPaint.setTextSize(42);
-        t.setColor(Color.BLACK );
-        t.setTextSize(42);
-
-        String lotStr = "Lot # ";
-        String lot = item.getLot();
-
-        String name = item.getName();
-        Log.d("pic", item.getImageURLs().get(0));
-
-        String catStr = "Category: ";
-        String cat = item.getCategory();
-
-        String perStr = "Period: ";
-        String per = item.getPeriod();
-
-        String descStr = "Description: ";
-        String desc = item.getDescription();
-
-        Bitmap picBig = getBitmap(item.getImageURLs().get(0));
-        Bitmap pic = Bitmap.createScaledBitmap(picBig, imgWidth, imgHeight, false);
-        if(pic != null){
-            canvas.drawBitmap(pic, pagewidth/2-imgWidth/2, 100, paint);
-            Log.d("err", "making image");
-        }
-//        canvas.drawText(lotStr + lot, pagewidth/2-100, 150 + imgHeight, t);
-//        canvas.drawText(name, pagewidth/2-100, 200 + imgHeight, t);
-//        canvas.drawText(cat, pagewidth/2-100, 250 + imgHeight, t);
-//        canvas.drawText(per, pagewidth/2-100, 300 + imgHeight, t);
-
-        StaticLayout textLayout = new StaticLayout(desc + "aukwhfuiohawfuhifwauh more filler frnraodmm sodtoest mfeiller ieand eoteaxt isflelra sifdnwame", textPaint, canvas.getWidth()-100, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
-        StaticLayout t1 = new StaticLayout(lotStr + lot, t, canvas.getWidth()-100, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
-        StaticLayout t2 = new StaticLayout(name, t, canvas.getWidth()-100, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
-        StaticLayout t3 = new StaticLayout(catStr + cat, t, canvas.getWidth()-100, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
-        StaticLayout t4 = new StaticLayout(perStr + per, t, canvas.getWidth()-100, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
-
-        canvas.save();
-        canvas.translate(pagewidth/2-500, 150 + imgHeight);
-        t1.draw(canvas);
-        canvas.restore();
-
-        canvas.save();
-        canvas.translate(pagewidth/2-500, 200 + imgHeight);
-        t2.draw(canvas);
-        canvas.restore();
-
-        canvas.save();
-        canvas.translate(pagewidth/2-500, 250 + imgHeight);
-        t3.draw(canvas);
-        canvas.restore();
-
-        canvas.save();
-        canvas.translate(pagewidth/2-500, 300 + imgHeight);
-        t4.draw(canvas);
-        canvas.restore();
-
-        canvas.save();
-        canvas.translate(100, 400 + imgHeight);
-        textLayout.draw(canvas);
-        canvas.restore();
-
-        pdfDocument.finishPage(myPage);
-
-        File downloads = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        String nam = "test.pdf";
-
-        File file = new File(downloads, nam);
-        try {
-            FileOutputStream fos = new FileOutputStream(file);
-            pdfDocument.writeTo(fos);
-            pdfDocument.close();
-            fos.close();
-            Toast.makeText(getContext(), "written success", Toast.LENGTH_SHORT).show();
-        } catch (FileNotFoundException e) {
-            Log.d("ERRR1", "printing errror");
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            Log.d("ERRR2", "printing errror");
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static Bitmap getBitmap(String src){
-        try{
-            URL url = new URL(src);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            Bitmap pic = BitmapFactory.decodeStream(input);
-            return pic;
-        } catch(IOException e){
-            Log.d("errr", "error");
-            return null;
-        }
-    }
-
-
 }
 
---
+++ app/src/main/java/com/b07project2024/group1/ReportViewModel.java
@@ -0,0 +1,246 @@
+package com.b07project2024.group1;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.pdf.PdfDocument;
+import android.os.Environment;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReportViewModel extends ViewModel {
+    List<CatalogItem> catalog;
+    int pageHeight = 1920;
+    int pagewidth = 1080;
+    int imgHeight = 550;
+    int imgWidth = 700;
+    String fileName;
+
+    public ReportViewModel(){
+        this.catalog = new ArrayList<>();
+    }
+
+    public void getWholeCatalog(String param, String type){
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference ref = db.getReference().child("catalog");
+        fileName = "Catalog Report.pdf";
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<CatalogItem> list = new ArrayList<>();
+
+                for (DataSnapshot child: dataSnapshot.getChildren()) {
+                    CatalogItem item = child.getValue(CatalogItem.class);
+                    list.add(item);
+                }
+
+                CatalogItem item = new CatalogItem();
+
+                if(type.equals("lot")){item = generateByLot(list, param);}
+                else if(type.equals("name")){item = generateByName(list, param);}
+                else if(type.equals("category")){item = generateByCategory(list, param, false);}
+                else if(type.equals("categoryDP")){item = generateByCategory(list, param, true);}
+                else if(type.equals("period")){item = generateByPeriod(list, param, false);}
+                else if(type.equals("periodDP")){item = generateByPeriod(list, param, true);}
+                else if(type.equals("all")){item = generateAll(list, false);}
+                else if(type.equals("allDP")){item = generateAll(list, true);}
+
+                if(item == null){
+                    Log.d("error", "No items found in parameters");
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("error", "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+    }
+
+    private void generatePDF(List<CatalogItem> list, int items, int index, boolean dp) {
+        PdfDocument pdfDocument = new PdfDocument();
+        PdfDocument.PageInfo info = new PdfDocument.PageInfo.Builder(pagewidth, pageHeight, items).create();
+
+        if(index != -1){createPage(list.get(index), pdfDocument, info, 1, dp);}
+        else{
+            for(int i=0; i<items; i++){
+                createPage(list.get(i), pdfDocument, info, i, dp);
+            }
+        }
+
+        if(dp){fileName = fileName.replace(".pdf", "(DP Only).pdf");}
+        File downloads = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File file = new File(downloads, fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            pdfDocument.writeTo(fos);
+            pdfDocument.close();
+            fos.close();
+
+        } catch (FileNotFoundException e) {
+            Log.d("error", "error");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            Log.d("error", "error");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Bitmap getBitmap(String src){
+        try{
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap pic = BitmapFactory.decodeStream(input);
+            return pic;
+        } catch(IOException e){
+            Log.d("error", "error");
+            return null;
+        }
+    }
+
+    public void createPage(CatalogItem item, PdfDocument pdfDocument, PdfDocument.PageInfo info, int pageNum, boolean dp){
+        PdfDocument.Page myPage = pdfDocument.startPage(info);
+        Canvas canvas = myPage.getCanvas();
+
+        int height = imgHeight + 150;
+        TextPaint textPaint = new TextPaint();
+        TextPaint t = new TextPaint();
+
+        textPaint.setColor(Color.BLACK );
+        textPaint.setTextSize(34);
+        t.setColor(Color.BLACK );
+        t.setTextSize(38);
+
+        Bitmap picBig = getBitmap(item.getImageURLs().get(0));
+        Bitmap pic = Bitmap.createScaledBitmap(picBig, imgWidth, imgHeight, false);
+        if(pic != null){
+            canvas.drawBitmap(pic, pagewidth/2-imgWidth/2, 100, t);
+        }
+
+        if(!dp) {
+            StaticLayout t1 = new StaticLayout("Lot #" + item.getLot(), t, canvas.getWidth() - 100, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
+            StaticLayout t2 = new StaticLayout(item.getName(), t, canvas.getWidth() - 100, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
+            StaticLayout t3 = new StaticLayout("Category: " + item.getCategory(), t, canvas.getWidth() - 100, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
+            StaticLayout t4 = new StaticLayout("Period: " + item.getPeriod(), t, canvas.getWidth() - 100, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
+
+            canvas.save();
+            canvas.translate(pagewidth / 2 - 500, height);
+            height += 50;
+            t1.draw(canvas);
+            canvas.restore();
+
+            canvas.save();
+            canvas.translate(pagewidth / 2 - 500, height);
+            height += 50;
+            t2.draw(canvas);
+            canvas.restore();
+
+            canvas.save();
+            canvas.translate(pagewidth / 2 - 500, height);
+            height += 50;
+            t3.draw(canvas);
+            canvas.restore();
+
+            canvas.save();
+            canvas.translate(pagewidth / 2 - 500, height);
+            height += 75;
+            t4.draw(canvas);
+            canvas.restore();
+        }
+
+        StaticLayout textLayout = new StaticLayout(item.getDescription(), textPaint, canvas.getWidth()-100, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+        canvas.save();
+        canvas.translate(100, height);
+        textLayout.draw(canvas);
+        canvas.restore();
+
+        pdfDocument.finishPage(myPage);
+    }
+
+    public CatalogItem generateByLot(List<CatalogItem> list, String lot){
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).getLot().equals(lot)){
+                fileName = "Lot " + lot + " Report.pdf";
+                generatePDF(list, 1, i, false);
+                return list.get(i);
+            }
+        }
+        Log.d("error", "Not Found");
+        return null;
+    }
+
+    public CatalogItem generateByName(List<CatalogItem> list, String name){
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).getName().equals(name)){
+                fileName = name + " Report.pdf";
+                generatePDF(list, 1, i, false);
+                return list.get(i);
+            }
+        }
+        Log.d("error", "Not Found");
+        return null;
+    }
+
+    public CatalogItem generateByCategory(List<CatalogItem> list, String category, boolean dp){
+        int count = 0;
+        List<CatalogItem> newList = new ArrayList<>();
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).getCategory().equals(category)){
+                newList.add(list.get(i));
+                count++;
+            }
+        }
+        fileName = category + " Report.pdf";
+        generatePDF(newList, count, -1, dp);
+        return list.get(0);
+    }
+
+    public CatalogItem generateByPeriod(List<CatalogItem> list, String period, boolean dp){
+        int count = 0;
+        List<CatalogItem> newList = new ArrayList<>();
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i).getPeriod().equals(period)){
+                newList.add(list.get(i));
+                count++;
+            }
+        }
+        fileName = period + " Report.pdf";
+        generatePDF(newList, count, -1, dp);
+        return list.get(0);
+    }
+
+    public CatalogItem generateAll(List<CatalogItem> list, boolean dp){
+        generatePDF(list, list.size(), -1, dp);
+        return list.get(0);
+    }
+
+
+}
